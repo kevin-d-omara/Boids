62f38a1a473d2f94fcc81d98523e4e3f9313d33d
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-31 16:49:23 -0700

    Add obstacles and player spawned Boids

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinDOMara.Boids2D
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private GameObject boidPrefab;
+        [SerializeField] private Transform boidHolder;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var clickPosition = Input.mousePosition;
+                clickPosition.z = 10f;
+                clickPosition = Camera.main.ScreenToWorldPoint(clickPosition);
+
+                // Create Boid!
+                var instance = Instantiate(boidPrefab, clickPosition, Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boidHolder);
+            }
+        }
+    }
+}
