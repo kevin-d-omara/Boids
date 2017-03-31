4532d163765e5002daee91491d6038be7c38427e
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-31 11:32:00 -0700

    Add Vector2 cross product
    The cross product between two 2D-Vectors can be used to determine the
    direction of rotation between them. +1 == clockwise, -1 ==
    counter-clockwise, and 0 == co-linear.

---
+++ Assets/Scripts/Pure C#/Vector2Extension.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinDOMara.Boids2D
+{
+    /// <summary>
+    /// Extension methods for Unity's Vector2 class.
+    /// </summary>
+    public static class Vector2Extension
+    {
+        /// <summary>
+        /// Return the rotation direction (clockwise, counter-clockwise, co-linear) between two
+        /// vectors via the cross product.
+        /// </summary>
+        /// <param name="lhs">Left hand side of cross product.</param>
+        /// <param name="rhs">Right hand side of cross product.</param>
+        /// <returns>+1 if clockwise, -1 if counter-clockwise, 0 if co-linear.</returns>
+        public static float RotateTo(this Vector2 lhs, Vector2 rhs)
+        {
+            var result = lhs.x * rhs.y - rhs.x * lhs.y;
+            
+            // Clockwise.
+            if (result > 0)
+            {
+                return 1f;
+            }
+            // Counter-clockwise.
+            else if (result < 0)
+            {
+                return -1f;
+            }
+            // Co-linear.
+            else
+            {
+                return 0f;
+            }
+        }
+    }
+}
