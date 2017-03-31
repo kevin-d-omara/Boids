0af460eb9eac31f2426107d33d0fc087cd86ee69
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-31 09:13:52 -0700

    Add basic interface for Boid
    Geometry is a wrapper for Physics2D overlap methods.

---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinDOMara.Boids2D
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class BoidController : MonoBehaviour
+    {
+        /// <summary>
+        /// Radius for interacting with other Boids
+        /// </summary>
+        public static float radius = 2f;
+
+        /// <summary>
+        /// Constant propulsion force.
+        /// </summary>
+        public static float speed = 10f;
+
+        /// <summary>
+        /// Forward direction of Boid.
+        /// </summary>
+        public Vector2 Heading
+        {
+            get
+            {
+                return transform.right;
+            }
+        }
+
+        private Rigidbody2D rigidBody;
+
+        private void Awake()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            var steerAngle = Vector2.zero;
+
+            // Determine new heading.
+            AvoidCollision();
+            AlignWithFlock();
+            MoveToFlock();
+
+            // Rotate heading by steerAngle ("pressure").
+
+            // Move forward.
+            rigidBody.velocity = speed * Heading;
+        }
+
+        private void AvoidCollision()
+        {
+            // Sum inverse distance of flock-mates
+            // Convert to steer angle pressure
+        }
+
+        private void AlignWithFlock()
+        {
+            // GetFlockAlignment()
+            // Calculate delta w/ self
+            // Convert to steer angle pressure
+        }
+
+        private void MoveToFlock()
+        {
+            // GetFlockPosition()
+            // Calculate delta w/ self
+            // Convert to steer angle pressure
+        }
+
+        private Vector2 GetAvoidance()
+        {
+            return Vector2.zero;
+        }
+
+        private Vector2 GetFlockAlignment()
+        {
+            return Vector2.zero;
+        }
+
+        private Vector2 GetFlockPosition()
+        {
+            return Vector2.zero;
+        }
+
+
+
+        /// <summary>
+        /// Return all BoidController components within the radius.
+        /// </summary>
+        private IList<BoidController> GetBoidsWithin(float radius)
+        {
+            var neighbors = Geometry.FindComponentsInCircle<BoidController>(transform.position, radius);
+            neighbors.Remove(this);
+
+            return neighbors;
+        }
+    }
+}
---
+++ Assets/Scripts/Pure C#/Geometry.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinDOMara.Boids2D
+{
+    public static class Geometry
+    {
+        /// <summary>
+        /// Return all GameObjects within a circle at the position. Discludes objects on the
+        /// IgnoreRaycastLayer.
+        /// </summary>
+        /// <returns>Array of all GameObjects within a circle at the position.</returns>
+        public static GameObject[] FindObjectsInCircle(Vector2 point, float radius)
+        {
+            var hits = Physics2D.OverlapCircleAll(point, radius);
+
+            GameObject[] objects = new GameObject[hits.Length];
+            for (int i = 0; i < hits.Length; ++i)
+            {
+                objects[i] = hits[i].gameObject;
+            }
+            return objects;
+        }
+
+        /// <summary>
+        /// Return all components (objects) of type T within a circle at the position. Discludes
+        /// objects on the IgnoreRayCastLayer.
+        /// </summary>
+        /// <returns>List of all Components within a circle at the position.</returns>
+        public static IList<T> FindComponentsInCircle<T>(Vector2 point, float radius)
+        {
+            var components = new List<T>();
+
+            var gameObjects = FindObjectsInCircle(point, radius);
+            foreach (GameObject gameObject in gameObjects)
+            {
+                var component = gameObject.GetComponent<T>();
+                if (component != null)
+                {
+                    components.Add(component);
+                }
+            }
+
+            return components;
+        }
+    }
+}
