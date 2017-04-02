d10df9dade98dc8ade238946926b34d71210c2d6
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-04-02 14:52:51 -0700

    Add moving 3D Boid

---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinDOMara.Boids3D
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class BoidController : MonoBehaviour
+    {
+        [Header("Boid Characteristics")]
+        public float flockRadius = 5f;
+        public float constantSpeed = 5f;
+
+        [Header("Steering Behavior")]
+        public float separationWeight = 1.0f;
+        public float alignmentWeight = 1.0f;
+        public float cohesionWeight = 1.0f;
+        public float waypointWeight = 1.0f;
+
+        /// <summary>
+        /// Forward direction of the Boid.
+        /// </summary>
+        public Vector3 Heading { get { return transform.up; } }
+
+        private Rigidbody rigidBody;
+
+        private void Awake()
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            var steeringPressure = Vector3.zero;
+            var flock = GetBoidsWithin(flockRadius);
+
+            // Determine new heading.
+            //steeringPressure += GetSeparationPressure(flock) * separationWeight;
+            //steeringPressure += GetAlignmentPressure(flock) * alignmentWeight;
+            //steeringPressure += GetCohesionPressure(flock) * cohesionWeight;
+            //steeringPressure += GetWaypointPressure(waypoint) * waypointWeight;
+            //RotateByPressure(steeringPressure);
+
+            // Move forward.
+            rigidBody.velocity = constantSpeed * Heading;
+        }
+
+        /// <summary>
+        /// Return all BoidController components within the radius.
+        /// </summary>
+        private IList<BoidController> GetBoidsWithin(float radius)
+        {
+            var neighbors = Geometry.FindComponentsInSphere<BoidController>(transform.position,
+                radius);
+            neighbors.Remove(this);
+
+            return neighbors;
+        }
+    }
+}
---
+++ Assets/Scripts/Pure C#/Geometry.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinDOMara.Boids3D
+{
+    public class Geometry : MonoBehaviour
+    {
+        /// <summary>
+        /// Return all GameObjects within a sphere at the position. Discludes objects on the
+        /// IgnoreRaycastLayer.
+        /// </summary>
+        public static GameObject[] FindObjectsInSphere(Vector3 point, float radius)
+        {
+            var hits = Physics.OverlapSphere(point, radius);
+
+            GameObject[] objects = new GameObject[hits.Length];
+            for (int i = 0; i < hits.Length; ++i)
+            {
+                objects[i] = hits[i].gameObject;
+            }
+            return objects;
+        }
+
+        /// <summary>
+        /// Return all components (objects) of type T within a sphera at the position. Discludes
+        /// objects on the IgnoreRayCastLayer.
+        /// </summary>
+        public static IList<T> FindComponentsInSphere<T>(Vector3 point, float radius)
+        {
+            var components = new List<T>();
+
+            var gameObjects = FindObjectsInSphere(point, radius);
+            foreach (GameObject gameObject in gameObjects)
+            {
+                var component = gameObject.GetComponent<T>();
+                if (component != null)
+                {
+                    components.Add(component);
+                }
+            }
+
+            return components;
+        }
+    }
+}
