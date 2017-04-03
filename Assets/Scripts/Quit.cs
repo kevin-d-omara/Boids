431a326cefa65326f6efc346bdb39ae0a24bd8cc
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-04-02 20:20:58 -0700

    Add Quit button

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KevinDOMara.Boids3D
+{
+    public class Quit : MonoBehaviour
+    {
+        public void OnSelectQuit()
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
